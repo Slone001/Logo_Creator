ed0aed873913690a8d35ab3618c14432910caacf
MarcSV <MarcStVogt@gmail.com>
2024-12-12 13:45:41 +0100

    added lego_edit.py file

---
+++ data/logo_handler/logo_edit.py
@@ -0,0 +1,11 @@
+from PIL import Image, ImageDraw, ImageFont
+
+
+def change_logo_color():
+    logoname = input("Logo name: ")
+    logo = Image.open(logoname).convert("RGBA")
+    new_color = (255, 0, 0, 255)  # Red (R, G, B, A)
+    colored_logo = Image.new("RGBA", logo.size, new_color)
+    logo_with_new_color = Image.composite(colored_logo, logo, logo)
+    logo_with_new_color.save("finished_logos/colored_logo.png")
+    print("Logo color changed and saved as 'colored_logo.png'")

ed8056ef624fb62655129716752e0ff87216e949
Slone001 <MarcStVogt@gmail.com>
2024-12-18 17:32:40 +0100

    added Class for logo-handling vars

--- data/logo_handler/__init__.py
+++ data/logo_handler/__init__.py
@@ -2,3 +2,11 @@ from .checks import *
 from .thread_handler import *
 from .season_logos import *
 from .logo_edit import *
+
+
+class logo_handler:
+
+    size = 200
+    font_path = "data/files/Hitmo2.0-Bold.ttf"
+
+

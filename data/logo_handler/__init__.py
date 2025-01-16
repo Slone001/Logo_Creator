35fe6fab01852a1630b6b57f6456796b6a6f27dc
MarcSV <MarcStVogt@gmail.com>
2025-01-16 09:27:58 +0100

    added vars

--- data/logo_handler/__init__.py
+++ data/logo_handler/__init__.py
@@ -1,12 +1,12 @@
 from .checks import *
-from .thread_handler import *
 from .season_logos import *
 from .logo_edit import *
+from .logo_handling import preview, logo_gen, imageQue
 
 
 class logo_handler:
 
     size = 200
-    font_path = "data/files/Hitmo2.0-Bold.ttf"
-
+    font_path = "F:/Programmieren/python/FruchtLabor/Logo-Creator/data/files/Hitmo2.0-Bold.ttf"
+    t_high = 1720
 
